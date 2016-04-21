d5334f090270835ca98bef077870df081237a3c0
Axel Bergh <abergh@despegar.com>
2016-04-21 14:56:08 -0300

    Changed key to integer

--- src/main/scala/com/despegar/p13n/kafka/producer/ExampleProducer.scala
+++ src/main/scala/com/despegar/p13n/kafka/producer/ExampleProducer.scala
@@ -15,14 +15,14 @@ object ExampleProducer extends App {
 //  props.put("batch.size", 16384: java.lang.Integer)
 //  props.put("linger.ms", 1: java.lang.Integer)
 //  props.put("buffer.memory", 33554432: java.lang.Integer)
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  val producer: Producer[String, String] = new KafkaProducer(props)
+  val producer: Producer[Int, String] = new KafkaProducer(props)
 
   Iterator.continually(StdIn.readLine)
     .takeWhile(_.nonEmpty).foreach { line =>
-      val response = producer.send(new ProducerRecord[String, String]("testTopic", line.hashCode().toString(), line))
+      val response = producer.send(new ProducerRecord[Int, String]("testTopic", line.hashCode(), line))
       val metadata = response.get(10, TimeUnit.SECONDS)
       println(s"offset: ${metadata.offset}, partition: ${metadata.partition}, topic: ${metadata.topic}")
     }
